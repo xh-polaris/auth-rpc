3bcdde1a210f6107440e54b911e6b7ef28ceebba
foliet <foliet@rainspace.cn>
2022-10-13 01:38:38 +0800

    test: 添加单元测试和mock代码

---
+++ rpc/internal/logic/set_password_logic_test.go
@@ -0,0 +1,73 @@
+package logic
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/xh-polaris/account-svc/mock/mockmodel"
+	"github.com/xh-polaris/account-svc/model"
+	"github.com/xh-polaris/account-svc/rpc/errorx"
+	"github.com/xh-polaris/account-svc/rpc/internal/config"
+	"github.com/xh-polaris/account-svc/rpc/internal/svc"
+	"github.com/xh-polaris/account-svc/rpc/pb"
+)
+
+func TestSetPasswordLogic_SetPassword(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	r, err := miniredis.Run()
+	assert.Nil(t, err)
+
+	userModel := mockmodel.NewMockUserModel(ctrl)
+	svcCtx := &svc.ServiceContext{
+		Config:    config.Config{},
+		UserModel: userModel,
+		Redis:     redis.New(r.Addr()),
+	}
+	ctx := context.Background()
+	l := NewSetPasswordLogic(ctx, svcCtx)
+
+	t.Run("no such user", func(t *testing.T) {
+		userModel.EXPECT().
+			FindOne(gomock.Any(), "123").
+			Return(nil, model.ErrNotFound).
+			Times(1)
+
+		_, err := l.SetPassword(&pb.SetPasswordReq{
+			UserId:   "123",
+			Password: "",
+		})
+		assert.Equal(t, errorx.ErrNoSuchUser, err)
+	})
+	t.Run("valid request", func(t *testing.T) {
+		id := primitive.NewObjectID()
+		userModel.EXPECT().
+			FindOne(gomock.Any(), id.Hex()).
+			Return(&model.User{
+				ID:       id,
+				UpdateAt: time.Now(),
+				CreateAt: time.Now(),
+				Password: "",
+				Auth:     nil,
+			}, nil).
+			Times(1)
+		userModel.EXPECT().
+			Update(gomock.Any(), gomock.Any()).
+			Return(nil).
+			Times(1)
+
+		resp, err := l.SetPassword(&pb.SetPasswordReq{
+			UserId:   id.Hex(),
+			Password: "",
+		})
+		assert.Nil(t, err)
+		assert.NotNil(t, resp)
+	})
+}
---
+++ rpc/internal/logic/sign_in_logic_test.go
@@ -0,0 +1,161 @@
+package logic
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/xh-polaris/account-svc/mock/mockmodel"
+	"github.com/xh-polaris/account-svc/model"
+	"github.com/xh-polaris/account-svc/rpc/errorx"
+	"github.com/xh-polaris/account-svc/rpc/internal/config"
+	"github.com/xh-polaris/account-svc/rpc/internal/svc"
+	"github.com/xh-polaris/account-svc/rpc/pb"
+)
+
+func TestSignInLogic_SignIn(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	r, err := miniredis.Run()
+	assert.Nil(t, err)
+
+	userModel := mockmodel.NewMockUserModel(ctrl)
+	svcCtx := &svc.ServiceContext{
+		Config:    config.Config{},
+		UserModel: userModel,
+		Redis:     redis.New(r.Addr()),
+	}
+	ctx := context.Background()
+	l := NewSignInLogic(ctx, svcCtx)
+
+	t.Run("invalid auth type", func(t *testing.T) {
+		_, err := l.SignIn(&pb.SignInReq{
+			AuthType:  "gitlab",
+			AuthValue: "12306",
+			Password:  "",
+			Options:   nil,
+		})
+		assert.Equal(t, errorx.ErrInvalidArgument, err)
+	})
+	t.Run("auth by phone or email", func(t *testing.T) {
+		err := svcCtx.Redis.Hset(VerifyCodeKey, "123@abc.com", "66666")
+		assert.Nil(t, err)
+
+		t.Run("no such user", func(t *testing.T) {
+			userModel.EXPECT().
+				FindOneByAuth(gomock.Any(), model.Auth{Type: "phone", Value: "12306"}).
+				Return(nil, model.ErrNotFound).
+				Times(1)
+
+			_, err := l.SignIn(&pb.SignInReq{
+				AuthType:  "phone",
+				AuthValue: "12306",
+				Password:  "",
+				Options:   nil,
+			})
+			assert.Equal(t, errorx.ErrNoSuchUser, err)
+		})
+		t.Run("wrong password", func(t *testing.T) {
+			userModel.EXPECT().
+				FindOneByAuth(gomock.Any(), model.Auth{Type: "phone", Value: "12306"}).
+				Return(&model.User{
+					ID:       primitive.NewObjectID(),
+					UpdateAt: time.Now(),
+					CreateAt: time.Now(),
+					Password: "$2a$10$KTaZRvmPE2MUfVOhjofOou8UgKAZEIkCftj3//iRFQCOpnAfLiDl2",
+					Auth:     []model.Auth{{"phone", "12306"}},
+				}, nil).
+				Times(2)
+
+			_, err := l.SignIn(&pb.SignInReq{
+				AuthType:  "phone",
+				AuthValue: "12306",
+				Password:  "",
+				Options:   nil,
+			})
+			assert.Equal(t, errorx.ErrWrongPassword, err)
+			_, err = l.SignIn(&pb.SignInReq{
+				AuthType:  "phone",
+				AuthValue: "12306",
+				Password:  "123",
+				Options:   nil,
+			})
+			assert.Equal(t, errorx.ErrWrongPassword, err)
+		})
+		t.Run("auth by email and password", func(t *testing.T) {
+			userModel.EXPECT().
+				FindOneByAuth(gomock.Any(), model.Auth{Type: "email", Value: "123@abc.com"}).
+				Return(&model.User{
+					ID:       primitive.NewObjectID(),
+					UpdateAt: time.Now(),
+					CreateAt: time.Now(),
+					Password: "$2a$10$KTaZRvmPE2MUfVOhjofOou8UgKAZEIkCftj3//iRFQCOpnAfLiDl2",
+					Auth:     []model.Auth{{"email", "123@abc.com"}},
+				}, nil).
+				Times(1)
+
+			resp, err := l.SignIn(&pb.SignInReq{
+				AuthType:  "email",
+				AuthValue: "123@abc.com",
+				Password:  "123456Abc.",
+				Options:   nil,
+			})
+			assert.Nil(t, err)
+			assert.NotNil(t, resp)
+		})
+		t.Run("auth by email and verify code", func(t *testing.T) {
+			userModel.EXPECT().
+				FindOneByAuth(gomock.Any(), model.Auth{Type: "email", Value: "123@abc.com"}).
+				Return(nil, model.ErrNotFound).
+				Times(1)
+			userModel.EXPECT().
+				Insert(gomock.Any(), gomock.Any()).
+				Return(nil).
+				Times(1)
+			userModel.EXPECT().
+				FindOneByAuth(gomock.Any(), model.Auth{Type: "email", Value: "123@abc.com"}).
+				Return(&model.User{
+					ID:       primitive.NewObjectID(),
+					UpdateAt: time.Now(),
+					CreateAt: time.Now(),
+					Password: "$2a$10$vJaijEGmaM4hgMF/55heder6dsEh7B6P8SdMnoDOMbRCJtBv6xD32",
+					Auth:     []model.Auth{{"email", "123@abc.com"}},
+				}, nil).
+				Times(1)
+
+			resp, err := l.SignIn(&pb.SignInReq{
+				AuthType:  "email",
+				AuthValue: "123@abc.com",
+				Password:  "123321",
+				Options:   []string{"66666"},
+			})
+			assert.Nil(t, err)
+			assert.NotNil(t, resp)
+			resp, err = l.SignIn(&pb.SignInReq{
+				AuthType:  "email",
+				AuthValue: "123@abc.com",
+				Password:  "241312",
+				Options:   []string{"66666"},
+			})
+			assert.Nil(t, err)
+			assert.NotNil(t, resp)
+		})
+	})
+	t.Run("auth by wechat", func(t *testing.T) {
+		t.Run("invalid options", func(t *testing.T) {
+			_, err := l.SignIn(&pb.SignInReq{
+				AuthType:  "wechat",
+				AuthValue: "12138",
+				Password:  "",
+				Options:   nil,
+			})
+			assert.Equal(t, errorx.ErrInvalidArgument, err)
+		})
+	})
+}
